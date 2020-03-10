b52c49688b2080f6d9386e3db425438c7ccc4618
maximkosheev <maximkosheev@mail.ru>
2020-03-10 08:01:46 +0500

    Добавить данные для теста

--- manager/backend/src/test/resources/db/testdata/afterMigrate.sql
+++ manager/backend/src/test/resources/db/testdata/afterMigrate.sql
@@ -1,1 +1,11 @@
-INSERT INTO ref_city ("name") VALUES('Озерск');
+insert into op_domofon(model, description, street_id, house, letter, building, porch, mounting_dt)
+values('domofon_model_1', '', 1, '1', '', '', '1', now());
+
+INSERT INTO op_client
+(user_id, fio, street_id, house, letter, building, room, phone, description, fsb, gorod, create_dt)
+VALUES(3, 'Тест Тест Тестович', 3, '1', 'A', '2', '131', '+79085898365', '', true, true, now());
+
+INSERT INTO op_account
+(client_id, domofon_id, account, has_device, device_switch_off, room, con_dt)
+VALUES(1, 1, 'QWERTY1', true, false, '12', now());
+
