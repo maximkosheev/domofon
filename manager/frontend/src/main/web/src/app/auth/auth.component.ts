6d30a4c61e5afd1ec660b5f254ae09d2a4212edb
maximkosheev <maximkosheev@mail.ru>
2020-04-08 00:52:42 +0500

    Заново начала переводить верстку на bootstrap 4

--- manager/frontend/src/main/web/src/app/auth/auth.component.ts
+++ manager/frontend/src/main/web/src/app/auth/auth.component.ts
@@ -5,7 +5,7 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css'],
+  styleUrls: ['./auth.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AuthComponent implements OnInit {
